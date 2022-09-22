d533259346d3c6bbd430a017e221cd60291ce767
Dmitrii <shokilla@mail.ru>
2022-09-22 11:30:45 +0400

    added dateIn

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import { renderSearchStubBlock } from './search-results.js'
 import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
 
+//даты для функции
+const dateIn = '25-09-2022';
+const dateOut = '25-11-2022';
+
+
 window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('Dima Shik','/img/avatar.png', 120)
-  renderSearchFormBlock()
+  renderUserBlock('Dima Shik','/img/dimaAva.jpeg', 120)
+  renderSearchFormBlock(dateIn, dateOut)
   renderSearchStubBlock()
   renderToast(
     {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
--- src/search-form.ts
+++ src/search-form.ts
@@ -1,23 +1,48 @@
 import { renderBlock } from './lib.js'
 
-export function renderSearchFormBlock() {
-  function addZero(num) {
-    if (num >= 0 && num <= 9) {
-      return '0' + num;
-    } else {
-      return num;
-    }
+function addZero(num) {
+  if (num >= 0 && num <= 9) {
+    return '0' + num;
+  } else {
+    return num;
   }
+}
+  
+
+export function renderSearchFormBlock(dateIn, dateOut) {
+
+  const dateCheckIn = dateIn.split('-').reverse().join('-');
+  const datCheckOut = dateIn.split('-').reverse().join('-');
+  // console.log('dateIn: ', dateIn111);
+  // console.log('dateOut: ',dateIn222 );
   //
   const date = new Date();
   const currentDate = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`
-  const startRentDate = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate() + 1)}`
+  let startRentDate = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate() + 1)}`
   
   const lastDay = new Date()
   lastDay.setFullYear(date.getFullYear(), date.getMonth() + 2, 0)
   const maxDate = `${lastDay.getFullYear()}-${addZero(lastDay.getMonth() + 1)}-${lastDay.getDate()}`
+ let checkOutDate = '';
   
-  let checkOutDate = ''  
+  // console.log('maxDay: ', maxDate);
+
+  if (dateCheckIn > currentDate && dateCheckIn <= maxDate) {
+    startRentDate = dateCheckIn;
+    console.log('startDate: ', startRentDate);
+  }
+
+ 
+
+  function handlerSubmitForm(event) {
+    event.preventDefault();
+    const checkin = form.checkin.value;
+    const checkInArr = checkin.split('-')
+  
+    const checkinDate = new Date(checkInArr[0], Number(checkInArr[1]) - 1, Number(checkInArr[2]) + 2)
+    checkOutDate = `${checkinDate.getFullYear()}-${addZero(checkinDate.getMonth() + 1)}-${addZero(checkinDate.getDate())}`
+    form.checkout.min = checkOutDate;    
+  }
   
   //render HTML 
   renderBlock(
@@ -39,7 +64,7 @@ export function renderSearchFormBlock() {
         <div class="row">
           <div>
             <label for="check-in-date">Дата заезда</label>
-            <input id="check-in-date" type="date" min=${startRentDate} max=${maxDate} name="checkin" />
+            <input id="check-in-date" type="date" min=${startRentDate} max=${maxDate} name="checkin" value=${dateCheckIn}/>
           </div>
           <div>
             <label for="check-out-date">Дата выезда</label>
@@ -58,22 +83,9 @@ export function renderSearchFormBlock() {
     `
   )
 
+  //после рендера присваиваем переменной нужный нам блок и навешиваем событие
   const form = document.querySelector('form');
- 
-  
-  function handlerSubmitForm(event) {
-    event.preventDefault();
-    const checkin = form.checkin.value;
-    const checkInArr = checkin.split('-')
-  
-    const checkinDate = new Date(checkInArr[0], Number(checkInArr[1]) - 1, Number(checkInArr[2]) + 2)
-    checkOutDate = `${checkinDate.getFullYear()}-${addZero(checkinDate.getMonth() + 1)}-${addZero(checkinDate.getDate())}`
-    form.checkout.min = checkOutDate;    
-  }
   
   form.addEventListener('change', handlerSubmitForm)
-  // if (form !=null) {
     
-  // }
-  
 }
